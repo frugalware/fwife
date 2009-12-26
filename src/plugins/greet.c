d044bd56300578775ecd7a8518bffa48cb3ad6e3
Elentir <elentir@mailoo.org>
2009-12-26 17:22:45 +0100

    little fixes in greet plugin
    * fix includes
    * typo fix in welcome message

--- src/plugins/greet.c
+++ src/plugins/greet.c
@@ -25,7 +25,6 @@
 
 #include <stdio.h>
 #include <gtk/gtk.h>
-#include<string.h>
 
 #include "common.h"
 
@@ -56,10 +55,10 @@ plugin_t *info()
 GtkWidget *load_gtk_widget()
 {
 	GtkWidget *widget = gtk_label_new (NULL);
-	gtk_label_set_markup(GTK_LABEL(widget), _("<span face=\"Times New Roman 12\"><b>Welcome among the users of Frugalware!\n\n</b></span>\n"
+	gtk_label_set_markup(GTK_LABEL(widget), _("<b>Welcome among the users of Frugalware!\n\n</b>\n"
 							"The aim of creating Frugalware was to help you to do your work faster and simpler.\n"
-							" We hope that you will like our product.\n\n"
-        					"<span font_desc=\"Times New Roman italic 8\" foreground=\"#0000FF\">The Frugalware Developer Team</span>\n"));
+							"We hope that you will like our product.\n\n"
+        					"<span style=\"italic\" font=\"10\" foreground=\"#0000FF\">The Frugalware Developer Team</span>\n"));
 	
 	return widget;
 }
